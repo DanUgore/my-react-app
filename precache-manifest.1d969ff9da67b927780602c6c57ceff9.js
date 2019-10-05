55ce12cbcb0148cd9152983cfea2fd8003671984
Dan Ugore <danugore3268@gmail.com>
2019-10-05 13:14:21 -0400

    Updates

---
+++ precache-manifest.1d969ff9da67b927780602c6c57ceff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d053374bf6f667d6f15422a0ec7275",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "276d70f98f09d1c04447",
+    "url": "/my-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b4371c078661de1bd49",
+    "url": "/my-react-app/static/js/2.dd0037e7.chunk.js"
+  },
+  {
+    "revision": "276d70f98f09d1c04447",
+    "url": "/my-react-app/static/js/main.dd94986c.chunk.js"
+  },
+  {
+    "revision": "8a2968c07ff388781e6f",
+    "url": "/my-react-app/static/js/runtime-main.360a9ff8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
